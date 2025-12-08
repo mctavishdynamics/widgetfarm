f0c0a39c26cb11c34ccb7dc05662ed172e2844ea
Derek McTavish <derek@mctavishdynamics.com>
2025-12-09 00:59:46 +0700

    Adjust border-radius and add icon support to Button stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { expect, fn, userEvent } from "storybook/test";
 
+import { TbEye } from "react-icons/tb";
 import { Button } from "./Button";
 import { ButtonProvider } from "./ButtonProvider";
 import styles from "./styles/kinda-retro.module.css";
@@ -33,6 +34,22 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     children: "Primary Button",
+  },
+  play: async ({ canvas, args }) => {
+    const button = canvas.getByRole("button");
+    await userEvent.click(button);
+    await expect(args.onClick).toHaveBeenCalled();
+  },
+};
+
+export const WithIcon: Story = {
+  args: {
+    children: (
+      <>
+        <TbEye />
+        Primary Button
+      </>
+    ),
   },
   play: async ({ canvas, args }) => {
     const button = canvas.getByRole("button");
